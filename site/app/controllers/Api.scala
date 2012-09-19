6c038d91abc9cf703bf4299feac5d6e258e660f1
Matthew Robbins <mrrobbins@gmail.com>
2012-09-19 16:56:48 -0500

    jquery returns json object stubs

--- site/app/controllers/Api.scala
+++ site/app/controllers/Api.scala
@@ -7,6 +7,7 @@ import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
 import play.api.data.format.Formats._
+import play.api.libs.json._
 import models.Report
 
 object Api extends Controller {
@@ -29,5 +30,21 @@ object Api extends Controller {
 		Ok(report.toString)
 	}
 
+	def getAllMarkers = Action {
+		import Json.toJson
+		val allPoints = toJson(
+			Map(
+				"points" -> toJson(Seq(
+					toJson(Map("name" -> toJson("Crowded Denny Chimes"), "lat" -> toJson(33.210973), "lng" -> toJson(-87.546279))),
+					toJson(Map("name" -> toJson("SERC"), "lat" -> toJson(33.213998), "lng" -> toJson(-87.543624)))
+				))
+			)
+		)
+
+		Ok(allPoints)
+	}
+
 }
+
+
 
