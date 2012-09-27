a33c563ce7bb368d6532f7b0a769e0ab00e956ab
Matthew Robbins <mrrobbins@gmail.com>
2012-09-27 17:37:02 -0500

    Schema overhaul, fix class templates

--- site/app/models/Report.scala
+++ site/app/models/Report.scala
@@ -5,49 +5,78 @@ import anorm._
 import play.api.db.DB
 import play.api.Play.current
 
-case class ReportTemplate(
-	latitude: Double,
-	longitude: Double,
+case class CollectionTemplate(
+	gps_address: String,
 	address: String,
 	locationDescription: String,
 	notes: String,
   damageIndicator: Int,
-  degreeOfDamage: Int
+  degreeOfDamage: Int,
+	primaryImageId: Int,
+	secondaryImages: Seq[Int]
 )
 
-case class Report(
+case class Collection(
 	id: Long,
-	picturePath: Option[String],
 	time: Long,
-	latitude: Double,
-	longitude: Double,
+	gpsAddress: String,
 	address: String,
 	locationDescription: String,
 	notes: String,
 	damageIndicator: Int,
-	degreeOfDamage: Int
+	degreeOfDamage: Int,
+	primaryImage: (PendingImageTemplate),
+	secondaryImages: Seq[PendingImageTemplate)]
 )
 
+object Collection {
 
-object Report {
-
-	def insert(r: ReportTemplate) = DB.withConnection { c =>
-		val newReport = SQL(
+	def insert(r: CollectionTemplate): Option[Long] = DB.withTransaction { c =>
+		val newCollection = SQL(
 			"""
-				INSERT INTO report (time, latitude, longitude, address, location_description, notes, damage_indicator, degree_of_damage, picture_path) VALUES
-        (UNIX_TIMESTAMP(), {lat}, {long}, {addr}, {desc}, {notes}, {indicator}, {degree}, NULL)
+				INSERT INTO collection (time, gps_address, address, location_description, notes, damage_indicator, degree_of_damage, primary_image_id) 
+				VALUES (UNIX_TIMESTAMP(), {gpsAddr}, {addr}, {desc}, {notes}, {indicator}, {degree}, {priImg})
 			"""
 		).on(
-			"lat" -> r.latitude, 
-			"long" -> r.longitude, 
+			"gpsAddr" -> r.gpsAddress,
 			"addr" -> r.address, 
 			"desc" -> r.locationDescription,
 			"notes" -> r.notes,
       "indicator" -> r.damageIndicator,
-      "degree" -> r.degreeOfDamage
+      "degree" -> r.degreeOfDamage,
+			"priImg" -> r.primaryImageId
 		)
-		
-		newReport.executeUpdate()(c)
+
+		try {
+			val priKey = newCollection.executeInsert()(c)
+
+			for (pendingImg <- r.secondaryImage :+ r.primaryImage) {
+				val newCollectionImage = Sql(
+					"""
+						INSERT INTO collection_image (collection_id, image_id, damage_indicator, degree_of_damage, notes) 
+						VALUES ({cid}, {iid}, {di}, {dd}, {notes})
+					"""
+				).on(
+					"cid" -> priKey,
+					"iid" -> pendingImg.imageId,
+					"di" -> pendingImg.damageIndicator,
+					"dd" -> pendingImg.damageDegree,
+					"notes" -> pendingImg.notes
+				)
+				newCollectionImage.executeInsert()(c)
+			}
+
+			Some(priKey)
+		} catch {
+			case e: java.sql.SqlException => 
+				e.printStackTrace()
+				c.rollback()
+				None
+			case e: Exception =>
+				e.printStackTrace()
+				c.rollback()
+				None
+		}
 	}
 }
 
