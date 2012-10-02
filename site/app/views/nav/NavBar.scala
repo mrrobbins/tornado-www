39a5b292ea1c1a236c09497673eccbcf0ad84279
clhodapp <clhodapp1@gmail.com>
2012-10-01 23:00:49 -0500

    Search box on NavBar

--- site/app/views/nav/NavBar.scala
+++ site/app/views/nav/NavBar.scala
@@ -6,7 +6,7 @@ case class NavItem(name: String, url: String)
 object NavBar {
 
 	object Map extends NavItem("map", "/map")
-	object PhotoQueue extends NavItem("photo queue", "/photoqueue")
+	object PhotoQueue extends NavItem("photo queue", "javascript:void(0)")
 
 	val headerItems = Seq(Map, PhotoQueue)
 }
