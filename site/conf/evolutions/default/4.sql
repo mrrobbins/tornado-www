3a2c1b90976d698f13c5111401df75020f418748
Matthew Robbins <mrrobbins@gmail.com>
2012-10-02 20:37:37 -0500

    Added user foreign key constraint

--- site/conf/evolutions/default/4.sql
+++ site/conf/evolutions/default/4.sql
@@ -5,7 +5,9 @@ CREATE TABLE pending_image (
 	notes VARCHAR(10240) NOT NULL,
 	damage_indicator INTEGER NOT NULL,
 	degree_of_damage INTEGER NOT NULL,
-	FOREIGN KEY (image_id) REFERENCES image (id)
+	user_id INTEGER NOT NULL,
+	FOREIGN KEY (image_id) REFERENCES image (id),
+	FOREIGN KEY (user_id) REFERENCES user (id)
 );
 
 # --- !Downs
