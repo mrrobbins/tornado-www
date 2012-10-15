2804b5313910a1b7324eaaf9b780ea226a5e7c26
clhodapp <clhodapp1@gmail.com>
2012-10-15 00:33:47 -0500

    Automatically get bugfix updates of dependencies

--- site/project/Build.scala
+++ site/project/Build.scala
@@ -8,8 +8,8 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      "mysql" % "mysql-connector-java" % "5.1.21",
-			"org.scalatest" %% "scalatest" % "2.0.M4" % "test"
+      "mysql" % "mysql-connector-java" % "5.1.+",
+			"org.scalatest" %% "scalatest" % "2.0.+" % "test"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
