69f677683daa01c0e1877b07a2e78376b79474e7
Matthew Robbins <mrrobbins@gmail.com>
2012-10-22 19:14:19 -0500

    Time is now BIGINT

--- site/conf/evolutions/default/3.sql
+++ site/conf/evolutions/default/3.sql
@@ -3,7 +3,7 @@
 CREATE TABLE image (
 	id INTEGER PRIMARY KEY AUTO_INCREMENT,
 	picture_path VARCHAR(1024) NOT NULL,
-	time_captured INTEGER NOT NULL, -- UNIX time
+	time_captured BIGINT, -- UNIX time
 	latitude DOUBLE NOT NULL,
 	longitude DOUBLE NOT NULL,
 	user_id INTEGER NOT NULL,
