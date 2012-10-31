6bd2a4ea117b6bd1b37a969c124b9453b61bd1ca
clhodapp <clhodapp1@gmail.com>
2012-10-31 00:45:28 -0500

    Fix syntax error in SQL script

--- site/conf/evolutions/default/3.sql
+++ site/conf/evolutions/default/3.sql
@@ -4,7 +4,7 @@ CREATE TABLE image (
 	id BIGINT PRIMARY KEY AUTO_INCREMENT,
 	picture_path VARCHAR(1024) NOT NULL,
 	time_captured BIGINT, -- UNIX time in ms
-	time_uploaded, -- UNIX time in ms
+	time_uploaded BIGINT, -- UNIX time in ms
 	latitude DOUBLE,
 	longitude DOUBLE,
 	user_id BIGINT NOT NULL,
