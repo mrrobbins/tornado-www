9802d533407661f5b372b66dbce85b3a94eecdb6
clhodapp <clhodapp1@gmail.com>
2012-11-04 22:11:33 -0600

    Fix implicit passing of users to NavBar

--- site/app/views/nav/NavBar.scala
+++ site/app/views/nav/NavBar.scala
@@ -1,6 +1,8 @@
 
 package views.nav
 
+import models._
+
 case class NavItem(name: String, url: String)
 
 object NavBar {
@@ -12,3 +14,23 @@ object NavBar {
 
 	val headerItems = Seq(Map, PhotoUpload, PhotoQueue)
 }
+
+sealed trait NavOptUser {
+	val maybeUser: Option[User]
+}
+
+object NavOptUser {
+	private final class UserOptUser(u: User) extends NavOptUser {
+		val maybeUser = Some(u)
+	}
+	private final class OptUser(o: Option[User]) extends NavOptUser {
+		val maybeUser = o
+	}
+
+	implicit def wrapUser(implicit u: User): NavOptUser =
+		new UserOptUser(u)
+	implicit def direct(implicit o: Option[User]): NavOptUser =
+		new OptUser(o)
+
+}
+
