a469fbc45ab58a357c9dd5f5b4c540a7f6c9873e
clhodapp <clhodapp1@gmail.com>
2012-11-06 01:57:24 -0600

    Make Maps API actions async

--- site/app/controllers/Maps.scala
+++ site/app/controllers/Maps.scala
@@ -6,6 +6,8 @@ import jp.t2v.lab.play20.auth._
 import models._
 import play.api.libs.json._
 import play.api.libs.json.Json.toJson
+import play.api.libs.concurrent.Akka
+import play.api.Play.current
 
 object Maps extends Controller with Auth with AuthConfigImpl {
 
@@ -15,17 +17,16 @@ object Maps extends Controller with Auth with AuthConfigImpl {
 
 	object Api {
 
-		def balloon(imageId: Long) = Action {
-
-			val image = Image.all.filter(image => image.id == imageId).head
-			val content = views.html.balloon(image).toString
+		def balloon(imageId: Long) = Action { Async { Akka.future {
+				val image = Image.all.filter(image => image.id == imageId).head
+				val content = views.html.balloon(image).toString
 		
-			val contentJson = toJson(Map("content" -> toJson(content)))
-			
-			Ok(contentJson)	
-		}
+				val contentJson = toJson(Map("content" -> toJson(content)))
 
-		def allMarkers = Action {
+				Ok(contentJson)	
+		} } }
+
+		def allMarkers = Action { Async { Akka.future {
 
 			val allMarkers = Marker.all.map(marker =>
 				Map(
@@ -40,7 +41,7 @@ object Maps extends Controller with Auth with AuthConfigImpl {
 			val allMarkersJson = toJson(Map("markers" -> toJson(allMarkers)))
 
 			Ok(allMarkersJson)
-		}
+		} } }
 	}
 
 }
