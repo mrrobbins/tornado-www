2a09cb5f9e99358a0c95fdc4fd5d865f8e793573
clhodapp <clhodapp1@gmail.com>
2012-11-08 21:34:57 -0600

    Generalize ensuring* to allow specing db

--- site/app/models/ModelHelpers.scala
+++ site/app/models/ModelHelpers.scala
@@ -8,12 +8,15 @@ import play.api.db.DB
 
 object ModelHelpers {
 
-	def ensuringConnection[T](func: Connection => T)(implicit conn: Connection): T = {
+	def ensuringConnection[T](database: String)(func: Connection => T)(implicit conn: Connection): T = {
 		if (conn == null) DB.withConnection(func)
 		else func(conn)
 	}
 
-	def ensuringTransaction[T](func: Connection => T)(implicit trans: Connection): T = {
+	def ensuringConnection[T](func: Connection => T)(implicit conn: Connection): T =
+		ensuringConnection("default")(func)(conn)
+
+	def ensuringTransaction[T](database: String)(func: Connection => T)(implicit trans: Connection): T = {
 		if (trans == null || trans.getAutoCommit) DB.withTransaction { t =>
 				try {
 					val res = func(t)
@@ -26,5 +29,8 @@ object ModelHelpers {
 				}
 		} else func(trans)
 	}
+
+	def ensuringTransaction[T](func: Connection => T)(implicit trans: Connection): T =
+		ensuringTransaction("default")(func)(trans)
 
 }
